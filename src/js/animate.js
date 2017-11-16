646cf19ea486b2495207940a1e4455fd1f123a77
Jordan Irabor <vastcastiel@gmail.com>
2017-11-16 06:51:22 +0100

    initial commit

---
+++ src/js/animate.js
@@ -0,0 +1,17 @@
+const icons = document.getElementsByClassName("image")
+
+    setTimeout(function(){
+        icons[0].classList.add('wobble');    
+    }, 3000)
+
+    setTimeout(function(){
+        icons[1].classList.add('swing');    
+    }, 4000)
+
+    setTimeout(function(){
+        icons[2].classList.add('jello');    
+    }, 5000)
+
+    setTimeout(function(){
+        icons[3].classList.add('tada');    
+    }, 6000)
---
+++ src/js/app.js
@@ -0,0 +1,41 @@
+const clients = document.getElementById("clients");
+const hamburger = document.getElementsByClassName("hamburger")[0];
+const mobile_menu = document.getElementsByClassName("mobile_menu")[0];
+
+
+document.body.style.webkitTransform =  'scale(1)';
+document.body.style.msTransform =   'scale(100)';
+document.body.style.transform = 'scale(1)';
+document.body.style.zoom = screen.logicalXDPI / screen.deviceXDPI;
+
+
+clients.addEventListener('click', function(event){
+
+event.target.setAttribute("style","animation-name:remove_greyscale; animation-duration:1s; animation-fill-mode:forwards") ;
+
+})
+
+hamburger.addEventListener('click', function(event){
+
+    if(event.target.classList.contains("fa-bars")){
+
+        event.target.classList.remove("fa-bars") ;
+        
+        event.target.classList.add("fa-times") ;   
+
+        mobile_menu.setAttribute("style","animation-name:come; animation-duration:0.5s; animation-fill-mode:forwards;");     
+
+    } else {
+
+        event.target.classList.remove("fa-times");
+
+        event.target.classList.add("fa-bars");  
+
+        mobile_menu.setAttribute("style","animation-name:go; animation-duration:0.5s; animation-fill-mode:forwards;");     
+        
+        
+
+    }
+})
+
+
