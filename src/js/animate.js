7ebff39cfe8f2597caa7019801837ce9161b3749
Jordan Irabor <vastcastiel@gmail.com>
2018-01-11 20:16:34 +0100

    slides

--- src/js/animate.js
+++ src/js/animate.js
@@ -34,8 +34,6 @@
       });
 
 
-
-
    $(window).scroll(function(){
         var theLogos = $('.theLogos'),
             scroll = $(window).scrollTop();
@@ -72,3 +70,27 @@
         }
 
       });
+
+
+      $(document).ready(function(){
+        $('.customer-logos').slick({
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 1500,
+            arrows: false,
+            dots: false,
+            pauseOnHover: false,
+            responsive: [{
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 4
+                }
+            }, {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 3
+                }
+            }]
+        });
+    });
